827292546755995491938c9963b36652fa105009
Jason Kries <jkries@gmail.com>
2019-11-22 13:24:25 -0500

    Update filemanager.php

--- filemanager.php
+++ filemanager.php
@@ -1,4 +1,6 @@
 <?php
+$passWord = "admin";
+
 //Default Configuration
 $CONFIG = '{"lang":"en","error_reporting":false,"show_hidden":false,"hide_Cols":false,"calc_folder":false}';
 
@@ -21,8 +23,8 @@ $use_auth = true;
 // Users: array('Username' => 'Password', 'Username2' => 'Password2', ...)
 // Generate secure password hash - https://tinyfilemanager.github.io/docs/pwd.html
 $auth_users = array(
-    'admin' => '$2y$10$/K.hjNr84lLNDt8fTXjoI.DBp6PpeyoJ.mGwrrLuCZfAwfSAGqhOW', //admin@123
-    'user' => '$2y$10$Fg6Dz8oH9fPoZ2jJan5tZuv6Z4Kp7avtQ9bDfrdRntXtPeiMAZyGO' //12345
+    'admin' => password_hash($passWord, PASSWORD_DEFAULT),
+    'user' => password_hash('12345', PASSWORD_DEFAULT)
 );
 
 // Readonly users (username array)
